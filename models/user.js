f8113cdaf633165e58754c0f51cf85d388d8e9e8
snowlight-aemt <82430645+snowlight-aemt@users.noreply.github.com>
2022-12-19 21:30:24 +0900

    Sequelize 초기 형태

---
+++ models/user.js
@@ -0,0 +1,14 @@
+const Sequelize = require('sequelize');
+
+class User extends Sequelize.Model {
+    static initiate(sequelize) {
+        // 모델(테이블) 정보들
+        // 모델 관계
+    }
+
+    static asociate(db) {
+        
+    }
+}
+
+module.exports = User;
