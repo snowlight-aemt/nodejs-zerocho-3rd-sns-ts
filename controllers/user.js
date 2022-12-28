4a2bcf32780c5fce006cf17c8bbe5c3801990678
snowlight-aemt <82430645+snowlight-aemt@users.noreply.github.com>
2022-12-28 21:44:29 +0900

    Fix - await 키워드 실수

--- controllers/user.js
+++ controllers/user.js
@@ -4,7 +4,7 @@ exports.follow = async (req, res, next) => {
     try {
         const user = await User.findOne({ where: { id: req.user.id }});
         if (user) {
-            user.addFollowing(parseInt(req.params.id, 10));
+            await user.addFollowing(parseInt(req.params.id, 10));
             res.send('success');
         } else {
             res.status(404).send('no user');
